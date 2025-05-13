38503fe6c3880427c01d5ecad9b82e7911fb3dd6
sayli utekar <saylimule3@gmail.com>
2025-05-13 22:40:48 +0530

    add

--- src/ex_01_Java_Basics/Multiple_Main_Method.java
+++ src/ex_01_Java_Basics/Multiple_Main_Method.java
@@ -4,13 +4,10 @@ public class Multiple_Main_Method {
     public static void main(String[] args) {
 
     }
-//there can be multiple main methods but only one main method which JVM recognize
-    /** you can have multiple main functions, but only one function of main method is recognize by JVM
-    * should be present
-     * The concept of multiple main method is called as Main Method Overloading
-     **/
+
 
     public static void main(int args) {
+        //there can be multiple main methods but only one main method which JVM recognize
 
     }
 
