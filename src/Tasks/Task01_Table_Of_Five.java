f7ad8103ba1ae41c2bdf3034cbc26f056fb20660
sayli utekar <saylimule3@gmail.com>
2025-05-16 16:32:28 +0530

    add

--- src/Tasks/Task01_Table_Of_Five.java
+++ src/Tasks/Task01_Table_Of_Five.java
@@ -1,6 +1,6 @@
 package Tasks;
 
-public class Table_Of_Five {
+public class Task01_Table_Of_Five {
     public static void main(String[] args) {
         int num = 5;
                 for (int i = 1; i <= 10; i++){
