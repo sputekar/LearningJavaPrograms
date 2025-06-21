0002d90d382460153b4051a657f271fb95b75636
sayli utekar <saylimule3@gmail.com>
2025-06-21 11:48:00 +0530

    21 June 2025

---
+++ src/Coding_Challenge/Challenge_01.java
@@ -0,0 +1,14 @@
+package Coding_Challenge;
+
+public class Challenge_01 {
+    public static void main(String[] args) {
+        int intergerValue = 25;
+        double doubleValue = 3.14;
+        boolean isJavaFun = true;
+        char grade = 'A';
+        System.out.println("Integer:" + intergerValue);
+        System.out.println("Double:" + doubleValue);
+        System.out.println("Boolean:" + isJavaFun);
+        System.out.println("Character:" + grade);
+    }
+}
